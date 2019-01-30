8f21a6bc543da86693602a1337050f5eb441b952
Елизавета Рогачева <liza-97-97-97@mail.ru>
2019-01-30 17:42:32 +0500

    gulp plagins installation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,71 @@
-var gulp = require('gulp'),
-var concatcss = require('gulp-concat-css'),
-var minify = require('gulp-minify-css'),
-var rename = require('gulp-rename'),
-var uglify = require('gulp-uglify'),
-var autoprefixer = require('gulp-autoprefixer'),
-var sass = require('gulp-sass'),
-var imagemin = require('gulp-imagemin'),
-var browserSync = require('browser-sync');
+var {src, dest, series, parallel, watch} = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync'),
+postcss = require('gulp-postcss');
+autoprefixer = require('autoprefixer');
+cssnano = require('cssnano');
+uglify = require('gulp-uglify');
+imagemin = require('gulp-imagemin');
+
+var reload = browserSync.reload;
+
+var plugins = [
+  autoprefixer({browsers: ['last 2 version']}),
+  cssnano()
+];
+
+
+function styles() {
+  return src('./app/css/main.scss')
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(dest('./dist/css'))
+}
+
+function scripts() {
+  return gulp.src('app/js/**/*.js')
+    .pipe(uglify())
+    .pipe(dest('./dist/js'))
+}
+
+function images() {
+  return gulp.src('app/img/*.*')
+    .pipe(imagemin()) 
+    .pipe(dest('./dist/img'));
+}
+
+function copyHtml() {
+  return src('app/*.html')
+    .pipe(dest('./dist'))
+}
+
+function watcher() {
+  watch('app/css/**/*.scss', styles);
+  watch('app/*.html', copyHtml);
+  watch('app/js/**/*.js', scripts);
+  watch('app/img/*.*', imagemin);
+
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  browserSync.watch('./dist/**/*.*', browserSync.reload);
+}
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.imagemin = imagemin;
+exports.copyHtml = copyHtml;
+exports.serve = serve;
+
+exports.default = series(parallel(styles, scripts, imagemin, copyHtml), parallel(watcher, serve));
+
+
+
+
+
