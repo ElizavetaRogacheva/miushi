f2f60c69c33364d464fee0e72f8b507275c3fab4
Елизавета Рогачева <liza-97-97-97@mail.ru>
2019-02-16 13:54:24 +0500

    стилизация slick dots

--- app/js/slider.js
+++ app/js/slider.js
@@ -5,6 +5,7 @@ var prev = document.querySelector('.slider__arrow--prev');
 
 $('.slider__list').slick(
     {   autoplay: true,
+        autoplaySpeed: 3000,
         prevArrow: prev,
         nextArrow: next,
         dots: true,
